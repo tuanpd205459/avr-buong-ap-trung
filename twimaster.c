96d80645c8b5008e22ba51eca0c20fdca32b6caf
tuanpd205459 <98724940+tuanpd205459@users.noreply.github.com>
2023-12-20 16:36:15 +0700

    Update twimaster.c

--- twimaster.c
+++ twimaster.c
@@ -1,11 +1,4 @@
-/*************************************************************************
-* Title:    I2C master library using hardware TWI interface
-* Author:   Peter Fleury <pfleury@gmx.ch>  http://jump.to/fleury
-* File:     $Id: twimaster.c,v 1.3 2005/07/02 11:14:21 Peter Exp $
-* Software: AVR-GCC 3.4.3 / avr-libc 1.2.3
-* Target:   any AVR device with hardware TWI 
-* Usage:    API compatible with I2C Software Library i2cmaster.h
-**************************************************************************/
+
 #include <inttypes.h>
 #include <compat/twi.h>
 
